c18ac2ba177d3290337cb2dfd39428ef20d327c7
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-31 11:50:09 +0530

    modified card.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,6 +4,38 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
+
+const NativeSelectDemo=({label,values})=>{
+  console.log(values)
+  return (
+    
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+          {label}
+        </InputLabel>
+        
+        <NativeSelect
+          inputProps={{
+            name: 'quantity',
+            id: 'uncontrolled-native',
+          }}
+        >
+          {Array.from(Array(values),(e,i)=>{
+            return(
+              <option key={i+1 } value={i+1}>{i+1}</option>
+          )
+          })}
+          
+        </NativeSelect>
+      </FormControl>
+    </Box>
+  );
+}
 
 export default function Cards({FoodName,Text,image}) {
   return (
@@ -28,7 +60,9 @@ export default function Cards({FoodName,Text,image}) {
         <Button size="large" color="secondary">
           Share
         </Button>
+        <NativeSelectDemo label={"Quantiy"} values={6}/>
       </CardActions>
+
     </Card>
   );
 }
