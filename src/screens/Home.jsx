4223972f7bc6ea6884070fa3a22b8ec60d82b84a
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-02-01 20:48:23 +0530

    Cards modified

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import Cards from '../components/Cards'
+import Cards from '../components/card/Cards'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import Hero from '../components/Hero/Hero'
+import food1 from '../assets/food1.jpeg'
+import food2 from '../assets/food2.jpg'
+import food3 from '../assets/foood3.jpg'
+import food4 from '../assets/food4.jpg'
+import food5 from '../assets/food1.jpg'
+
+
 
 export default function Home() {
   return (
@@ -14,7 +21,7 @@ export default function Home() {
       <Hero/>
     </div>
      <div>
-        <Cards />
+        <Cards image={food1} FoodName={"Daal Makhni"} Text={"One of the delicious food in the world and you can find it more tasty "}/>
     </div>
     <div>
       <Footer/>
